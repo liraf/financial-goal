88037a9f64586f3af6394332f4a696f5817a963c
Felipe Lira <felipelira1908@hotmail.com>
2022-11-17 23:42:48 -0300

    feat: implement Header component

--- src/pages/SavingGoal/SavingGoal.tsx
+++ src/pages/SavingGoal/SavingGoal.tsx
@@ -1,7 +1,8 @@
 import "./SavingGoal.scss";
+import Header from "../../components/Header/Header";
 
 const SavingGoal = () => {
-  return <div className="savingGoal">saving goal</div>;
+  return <div className="savingGoal"><Header /></div>;
 };
 
 export default SavingGoal;
